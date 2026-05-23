0d6824263515a8d671eb54c5aca5ecb628bd8de8
iasminaiasmi <iasminabaicu@yahoo.com>
2026-05-23 15:09:06 +0300

    reparat indentarea

--- app.py
+++ app.py
@@ -87,20 +87,20 @@ except (KeyError, FileNotFoundError):
 def get_air_quality_category(pm25: float) -> dict:
     # === TODO 3 — YOUR CODE HERE ===
     if pm25 < 12:
-return {"label": "Good", "color": "#22C55E",
-"advice": "Air quality is great. Enjoy outdoor activities."}
-elif pm25 < 35:
-return {"label": "Moderate", "color": "#FFCC4E",
-"advice": "OK for most. Sensitive groups should limit prolonged exertion."}
-elif pm25 < 55:
-return {"label": "Unhealthy for Sensitive Groups", "color": "#FF9800",
-"advice": "Children, elderly, and asthmatics should avoid outdoor activity."}
-elif pm25 < 150:
-return {"label": "Unhealthy", "color": "#E53935",
-"advice": "Everyone should reduce outdoor exertion."}
-else:
-return {"label": "Hazardous", "color": "#7e22ce",
-"advice": "Stay indoors. Use air filtration if possible."}
+        return {"label": "Good", "color": "#22C55E",
+                "advice": "Air quality is great. Enjoy outdoor activities."}
+    elif pm25 < 35:
+        return {"label": "Moderate", "color": "#FFCC4E",
+            "advice": "OK for most. Sensitive groups should limit prolonged exertion."}
+    elif pm25 < 55:
+        return {"label": "Unhealthy for Sensitive Groups", "color": "#FF9800",
+            "advice": "Children, elderly, and asthmatics should avoid outdoor activity."}
+    elif pm25 < 150:
+        return {"label": "Unhealthy", "color": "#E53935",
+            "advice": "Everyone should reduce outdoor exertion."}
+    else:
+        return {"label": "Hazardous", "color": "#7e22ce",
+            "advice": "Stay indoors. Use air filtration if possible."}
 
 
 
